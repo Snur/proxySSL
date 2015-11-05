96f344ab47f440ea99d22ff650c77eb24d025382
admin <osoucy27@gmail.com>
2015-11-05 15:14:18 -0500

    Correction d’un petit bug.

--- compileNFQDAQ/commandToExecute.sh
+++ compileNFQDAQ/commandToExecute.sh
@@ -1,5 +1,6 @@
 sudo yum install flex libpcap-devel bison libmnl-devel libnfnetlink-devel libnetfilter_queue-devel libdnet-devel automake
 cd ../daq-2.0.6/
+sudo ln -s /usr/bin/aclocal /usr/bin/aclocal-1.15
 sudo ln -s /usr/bin/automake /usr/bin/automake-1.15
 ./configure --disable-afpacket-module --disable-dump-module --disable-ipfw-module --disable-ipq-module --disable-pcap-module --disable-netmap-module --enable-nfq-module
 make
