8356806c0bf7a90c63852a7f65ccf2e3a4c1fe7b
Bah <bahbobo93@yahoo.fr>
2015-12-05 05:47:17 +0000

    Permettre à l'utilisateur de rajouter ses propres tags et prise en compte de la base de données de tag.

--- interface/index.php
+++ interface/index.php
@@ -1,30 +1,42 @@
 <html>
   <head>
     <meta charset = "utf-8">
-    <title>Dark Net</title>
+    <title> Dark Net </title>
   </head>
   <body>
     <form method="post" action="traitement.php">
      
        <fieldset>
-           <legend>Sites web</legend> <!-- Titre du fieldset --> 
+           <legend> APPLICATIONS </legend>
            <p>
-             <label for="site">Quels sites souhaitez-vous bloquer ?</label><br />
-             <select name="url" id="url">
-                 <option value="fb">Facebook</option>
-                 <option value="ig">Instagram</option>
-                 <option value="a24">Africa24</option>              
-             </select>
+             <label for="site"> Quelles applications souhaitez-vous bloquer ? </label> <br />
+             <input type="text" id="app" name="app" placeholder="Séparer les par ';'" />
            </p>
        </fieldset>
      
        <fieldset>
-           <legend>Contenu à bloquer</legend> 
+           <legend> TAGS à BLOQUER </legend> 
            
-	   <label for="bloc">Que souhaitez-vous bloquer ? </label><br />
-	   <input type="checkbox" name="like" id="like" /><label for="like">Like</label><br>
-           <input type="checkbox" id="com" name="com" onclick="afficher(this)" /><label for="com">Commentaires</label><br>
-           <label for="comment">Contenu specifique:</label><input type="text" id="comment" name="comment" disabled="disabled"/>
+    	     <label for="block"> Que souhaitez-vous bloquer ? </label> <br />
+           <select name="tag" id="tag">
+        	     <option value="default"> Tags </option>";
+        	     <?php
+            		  $connect = mysql_connect('localhost','root','Centos@2015') or die("Erreur de connexion au serveur.");
+            		  mysql_select_db('darknet',$connect) or die ("Erreur lors de la connexion à la base de données");
+
+            		  $req = mysql_query("SELECT * FROM tag",$connect);
+            		  while($data = mysql_fetch_object($req)) {
+            		    // on affiche les informations de l'enregistrement en cours
+            		    echo "<option value=\"$data->nom_tag\"> $data->nom_app </option>";
+            	          }
+        	     ?>
+           </select>
+                 
+           <p>
+        		<label for="texte"> Aucun tag ne vous convient? Rajoutez-en </label> <br />
+        		<input type="text" id="ajout" name="ajout" placeholder="Séparer les par ';'" />
+        		<br /> <label for="ind"> Veuillez respecter le format: nom_application:tag </label> <br />
+           </p>
 				   
        </fieldset>
        <input type="submit" value="Générer le script" />
