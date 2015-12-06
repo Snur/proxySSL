cac77be386dce3c03a3fb25eda68a7300f8d8650
Bah <bahbobo93@yahoo.fr>
2015-12-06 21:31:19 +0000

    Prise en compte des appid de openappid et des tags rajoutés par l'utilisateur

--- interface/index.php
+++ interface/index.php
@@ -5,29 +5,20 @@
   </head>
   <body>
     <form method="post" action="traitement.php">
-     
-       <fieldset>
-           <legend> APPLICATIONS </legend>
-           <p>
-             <label for="site"> Quelles applications souhaitez-vous bloquer ? </label> <br />
-             <input type="text" id="app" name="app" placeholder="Séparer les par ';'" />
-           </p>
-       </fieldset>
-     
        <fieldset>
            <legend> TAGS à BLOQUER </legend> 
            
     	     <label for="block"> Que souhaitez-vous bloquer ? </label> <br />
-           <select name="tag" id="tag">
-        	     <option value="default"> Tags </option>";
+           <select name="tag[]" id="tag" MULTIPLE>
+        	     <option value="default"> Tags_OpenAppID </option>";
         	     <?php
-            		  $connect = mysql_connect('localhost','root','Centos@2015') or die("Erreur de connexion au serveur.");
+            		  $connect = mysql_connect('localhost','darknet_user','Centos@2015') or die("Erreur de connexion au serveur.");
             		  mysql_select_db('darknet',$connect) or die ("Erreur lors de la connexion à la base de données");
 
-            		  $req = mysql_query("SELECT * FROM tag",$connect);
+            		  $req = mysql_query("SELECT * FROM Tag_openappid",$connect);
             		  while($data = mysql_fetch_object($req)) {
             		    // on affiche les informations de l'enregistrement en cours
-            		    echo "<option value=\"$data->nom_tag\"> $data->nom_app </option>";
+            		    echo "<option value=\"$data->nom_tag\"> $data->nom_tag </option>";
             	          }
         	     ?>
            </select>
@@ -36,6 +27,16 @@
         		<label for="texte"> Aucun tag ne vous convient? Rajoutez-en </label> <br />
         		<input type="text" id="ajout" name="ajout" placeholder="Séparer les par ';'" />
         		<br /> <label for="ind"> Veuillez respecter le format: nom_application:tag </label> <br />
+			<select name="tagP[]" id="tagP" MULTIPLE>
+			  <option value="default"> Tags_perso </option>";
+	<?php
+			  $req = mysql_query("SELECT * FROM Tag_perso",$connect);
+            		    while($data = mysql_fetch_object($req)) {
+            		      // on affiche les informations de l'enregistrement en cours
+            		      echo "<option value=\"$data->nom_tag\"> $data->nom_app </option>";
+            	            }
+	?>
+			</select>
            </p>
 				   
        </fieldset>
