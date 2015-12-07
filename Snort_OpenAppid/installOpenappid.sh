506b176fdacccb2cd64bed3960857a13cd7a0db2
Bah <bahbobo93@yahoo.fr>
2015-12-07 00:40:35 +0000

    Utiliser le nouveau paquet rpm qui permet d'installer snort et openappid en même temps

--- Snort_OpenAppid/installOpenappid.sh
+++ Snort_OpenAppid/installOpenappid.sh
@@ -1,23 +1,7 @@
-#!/bin/bash
-sudo yum remove snort
-mkdir /home/admin/installs
-cd /home/admin/installs
-wget http://luajit.org/download/LuaJIT-2.0.2.tar.gz
-tar xzvf LuaJIT-2.0.2.tar.gz
-cd LuaJIT-2.0.2
-sudo make
-sudo make install
-cd ..
-wget https://www.snort.org/downloads/snort/snort-2.9.8.0.tar.gz
-tar -xvf snort-2.9.8.0.tar.gz
-cd  snort-2.9.8.0
-./configure --enable-sourcefire --enable-open-appid
-sudo make
-make install
-cd /etc
-cp attribute_table.dtd file_magic.conf unicode.map classification.config gen-msg.map reference.config threshold.conf /etc/snort/
+sudo yum install https://www.snort.org/downloads/snort/snort-openappid-2.9.8.0-1.centos7.x86_64.rpm
+sudo rm /etc/snort/snort.conf
+sudo cp ./snort.conf /etc/snort/snort.conf
 mkdir /usr/local/lib/openappid
 wget https://snort.org/downloads/openappid/2738 -O snort-openappid.tar.gz
 tar -xzvf ./snort-openappid.tar.gz
 mv odp /usr/local/lib/openappid
-
